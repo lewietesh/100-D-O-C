c6340feed3c5e9594e86fd0f6094cc60bef25afb
lewietesh <lewismutembei001@gmail.com>
2025-08-26 17:09:35 +0300

    resume inclusion

--- src/app/home/Blog.tsx
+++ src/app/home/Blog.tsx
@@ -32,7 +32,7 @@ interface ApiBlogPost {
   reading_time: number;
 }
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000";
 
 export default function Blog() {
   const [posts, setPosts] = useState<ApiBlogPost[]>([]);
